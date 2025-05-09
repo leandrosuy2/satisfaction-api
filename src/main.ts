6e1b6d79afd696e1aec8bb9ccf3fab0e874a82d1
Leandro Vinicius <leandrovinicius36@gmail.com>
2025-05-09 00:27:53 -0400

    atualizaçao

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,25 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true
   });
-  
+
   app.useGlobalPipes(new ValidationPipe());
-  
+
   const config = new DocumentBuilder()
     .setTitle('Satisfaction API')
     .setDescription('API for satisfaction survey system')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-    
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3005);
+  await app.listen(3005, '0.0.0.0');
 }
 bootstrap();
